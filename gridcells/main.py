17056b221d190e49fa0818e876b0724d3b12f4f7
Tomasz Roszczynialski <roszcz@gmail.com>
2022-08-03 22:10:22 +0200

    Add training loop

---
+++ gridcells/main.py
@@ -0,0 +1,44 @@
+import torch
+from tqdm import tqdm
+from glob import glob
+from torch.utils.data import DataLoader
+
+from gridcells.models import main as gridcell_models
+from gridcells.data.dataset import SelfLocationDataset
+from gridcells.training import epochs as training_epochs
+
+
+def main():
+    paths = glob('data/torch/*pt')
+    model = gridcell_models.WorstModel()
+    t_dataset = SelfLocationDataset(paths[:30])
+    v_dataset = SelfLocationDataset(paths[30:40])
+
+    train_loader = DataLoader(t_dataset, batch_size=1024, shuffle=True, num_workers=8)
+    validation_loader = DataLoader(v_dataset, batch_size=1024, shuffle=False, num_workers=8)
+
+    loss_fn = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.98), eps=1e-9)
+
+    n_epochs = 32
+
+    progress_bar = tqdm(range(n_epochs), total=n_epochs)
+    for epoch in progress_bar:
+        training_loss = training_epochs.train_epoch(
+            model=model,
+            data_loader=train_loader,
+            optimizer=optimizer,
+            partial_loss_fn=loss_fn,
+        )
+
+        validation_loss = training_epochs.validation_epoch(
+            model=model,
+            data_loader=validation_loader,
+            partial_loss_fn=loss_fn,
+        )
+        epoch_summary = f'Training loss: {training_loss:.2f}, validation loss: {validation_loss:.2f}'
+        progress_bar.set_description(epoch_summary)
+
+
+if __name__ == '__main__':
+    main()
--- gridcells/models/main.py
+++ gridcells/models/main.py
@@ -9,18 +9,25 @@ class WorstModel(nn.Module):
         super().__init__()
         self.fc1 = nn.Linear(203, 100)
         self.fc2 = nn.Linear(100, 100)
-        self.out_place = nn.Linear(100, 200)
-        self.out_head = nn.Linear(100, 100)
+        self.out_place_cells = nn.Linear(100, 200)
+        self.out_head_cells = nn.Linear(100, 100)
 
     def forward(self, init_pos: Tensor, init_hd: Tensor, ego_vel: Tensor) -> tuple[Tensor, Tensor]:
+        # Concatenate everything into a single vector
         head_rotation_speed = torch.arctan2(ego_vel[..., 1], ego_vel[..., 2])
         forward_speed = ego_vel[..., 0]
         x = torch.hstack([head_rotation_speed, forward_speed, init_hd, init_pos])
 
+        # Run it through a MLP
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
 
-        place_cells = self.out_place(x)
+        # Decode the output into two sets of "cells"
+        # Expected shape for position
+        place_cells = self.out_place_cells(x)
         place_cells = place_cells.view(-1, 100, 2)
-        head_cells = self.out_head(x)
+
+        # Expected shape for head direction
+        head_cells = self.out_head_cells(x)
+        head_cells = head_cells.view(-1, 100, 1)
         return place_cells, head_cells
