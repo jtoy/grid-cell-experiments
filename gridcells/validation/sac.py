7e001cf4f5aea35d69fec985069154f9396ece52
Tomasz Roszczynialski <roszcz@gmail.com>
2022-08-28 20:21:18 +0200

    Move existing pipeline to a separate module

---
+++ gridcells/validation/sac.py
@@ -0,0 +1,49 @@
+import scipy
+import numpy as np
+
+
+def calculate_sac(seq1):
+    """Calculating spatial autocorrelogram."""
+    seq2 = seq1
+
+    def filter2(b, x):
+        stencil = np.rot90(b, 2)
+        return scipy.signal.convolve2d(x, stencil, mode='full')
+
+    seq1 = np.nan_to_num(seq1)
+    seq2 = np.nan_to_num(seq2)
+
+    ones_seq1 = np.ones(seq1.shape)
+    ones_seq1[np.isnan(seq1)] = 0
+    ones_seq2 = np.ones(seq2.shape)
+    ones_seq2[np.isnan(seq2)] = 0
+
+    seq1[np.isnan(seq1)] = 0
+    seq2[np.isnan(seq2)] = 0
+
+    seq1_sq = np.square(seq1)
+    seq2_sq = np.square(seq2)
+
+    seq1_x_seq2 = filter2(seq1, seq2)
+    sum_seq1 = filter2(seq1, ones_seq2)
+    sum_seq2 = filter2(ones_seq1, seq2)
+    sum_seq1_sq = filter2(seq1_sq, ones_seq2)
+    sum_seq2_sq = filter2(ones_seq1, seq2_sq)
+    n_bins = filter2(ones_seq1, ones_seq2)
+    n_bins_sq = np.square(n_bins)
+
+    std_seq1 = np.power(
+        np.subtract(
+            np.divide(sum_seq1_sq, n_bins),
+            (np.divide(np.square(sum_seq1), n_bins_sq))), 0.5)
+    std_seq2 = np.power(
+        np.subtract(
+            np.divide(sum_seq2_sq, n_bins),
+            (np.divide(np.square(sum_seq2), n_bins_sq))), 0.5)
+    covar = np.subtract(
+        np.divide(seq1_x_seq2, n_bins),
+        np.divide(np.multiply(sum_seq1, sum_seq2), n_bins_sq))
+    x_coef = np.divide(covar, np.multiply(std_seq1, std_seq2))
+    x_coef = np.real(x_coef)
+    x_coef = np.nan_to_num(x_coef)
+    return x_coef
